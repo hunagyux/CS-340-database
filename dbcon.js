a22b522478361c2e888fba82d5dbbdb14f0de609
Shawn_huang <38021236+hunagyux@users.noreply.github.com>
2020-02-29 18:56:35 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_huangyux',
+  password        : '9257',
+  database        : 'cs340_huangyux'
 });
 module.exports.pool = pool;
